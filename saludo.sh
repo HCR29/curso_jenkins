b8d6bdff0e54a215f00eba633389aab5b06893dd
HCR <hebertchr95@gmail.com>
2025-06-21 15:45:25 -0500

    Prueba solo

--- saludo.sh
+++ saludo.sh
@@ -1,3 +1,3 @@
 echo "Hola mundo con jenkins & Github con sh y sublimetext"
 echo "Probando trigger desde Github y jenkins"
-echo "Probando trigeer con webhook desde git y usando ngrok "
+echo "Probando trigeer con webhook desde git y usando ngroks     "
