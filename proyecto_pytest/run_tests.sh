5ce92df28a34f408f81d538dc4de7a67f3046fa5
HCR <hebertchr95@gmail.com>
2025-06-22 03:56:48 -0500

    prueeba

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -6,11 +6,25 @@ if [!"-d venv"]; then
 fi
 source venv/bin/activate
 
-echo "instalando dependencias"
-pip install --upgrade pip
-pip install -r requirements.txt
+#Avtivar entorno virtual correctamente
+if[-f "venv/bin/avtivate"]; then
+	source venv/bin/activate
+elif[ -f "venv/Scripts/avtivate"]; then
+	source venv/Scripts/activate
+else
+	echo "Error : no se pudo activar el entorno virtual"
+	exit 1
+fi
 
+#Verificar si pip esta instalando correctamente
+
+echo "instalando dependencias"
+pip install --upgrade pip --break-system-packages
+pip install -r requirements.txt --break-system-packages
+
+mkdir -p reports
+# Ejecutar pruebas
 echo "Ejecutano pruebas con pytest"
-pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+venv/bin/python -m pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
 
 echo "pruebas finalizadas resultados en reports"
